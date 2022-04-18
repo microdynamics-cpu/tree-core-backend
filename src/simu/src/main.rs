2916b2d71a9f07ea328a7f36b0d95f85fe149349
maksyuki <maksyuki@126.com>
2022-04-18 22:14:49 +0800

    feat: add inter mode func

--- src/simu/src/main.rs
+++ src/simu/src/main.rs
@@ -3,10 +3,21 @@ use std::fs::File;
 use std::io::Read;
 use treecore_simu::core::Core;
 
+fn interactive_mode() {
+    println!("TreeCore RISCV ISA Simulator 0.0.1");
+    println!("[last-release] on Ubuntu 20.04 LTS");
+    println!("Type 'help' for more information.");
+    println!(">>>");
+    // loop {
+    //     println!(">>>");
+    // }
+}
+
 fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
-        eprint!("Usage: treecore-simu file.bin\n");
+        // eprint!("usage: treecore-simu file.bin\n");
+        interactive_mode();
         return Ok(());
     }
 
