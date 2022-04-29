01d94f48af77186c73d11a44567ecde691b018b4
maksyuki <maksyuki@126.com>
2022-04-29 09:32:45 +0800

    chore: add cust rv64i app and pass all tests

--- src/simu/scripts/unit-test.sh
+++ src/simu/scripts/unit-test.sh
@@ -9,12 +9,18 @@ END="\033[0m"
 ROOT_PATH=$(dirname $(readlink -f "$0"))/../dependency
 RISCV_TESTS_PATH=${ROOT_PATH}/riscv-tests
 RISCV_TESTS_BIN_PATH=${RISCV_TESTS_PATH}/build/share/riscv-tests/isa
+CUST_TESTS_BIN_PATH1=/root/Desktop/chisel/tree-core-cpu/rtl/dependency/am/riscv-tests/build/
+CUST_TESTS_BIN_PATH2=/root/Desktop/chisel/tree-core-cpu/rtl/dependency/am/am-kernels/tests/cpu-tests/build/
+
 PROGRAM=$(dirname $(readlink -f "$0"))/../target/debug/treecore_simu
+
 # test case
 RV32UI_P_TEST_BIN=`eval "find $RISCV_TESTS_BIN_PATH -type f -name \"rv32ui-p*\" ! -name \"*.dump\""`
 RV32UM_P_TEST_BIN=`eval "find $RISCV_TESTS_BIN_PATH -type f -name \"rv32um-p*\" ! -name \"*.dump\""`
 RV64UI_P_TEST_BIN=`eval "find $RISCV_TESTS_BIN_PATH -type f -name \"rv64ui-p*\" ! -name \"*.dump\""`
 RV64UM_P_TEST_BIN=`eval "find $RISCV_TESTS_BIN_PATH -type f -name \"rv64um-p*\" ! -name \"*.dump\""`
+RV64I_CUST_TEST_BIN1=`eval "find $CUST_TESTS_BIN_PATH1 -type f -name \"*-riscv64-mycpu.bin\" ! -name \"*.dump\""`
+RV64I_CUST_TEST_BIN2=`eval "find $CUST_TESTS_BIN_PATH2 -type f -name \"*-riscv64-mycpu.bin\" ! -name \"*.dump\""`
 
 unitTest() {
     printf "$INFO===RV32UI TESTS===$END\n"
@@ -43,6 +49,22 @@ unitTest() {
         val=`eval "basename $file"`
         printf "$INFO[%16s] $END" $val
         $PROGRAM --bin $file --xlen x64 --start-addr 0x1000 --end-inst 0x00000073
+    done
+
+    printf "$INFO===RV64I CUSTOM TESTS===$END\n"
+    for file in $RV64I_CUST_TEST_BIN1; do
+        val=`eval "basename $file"`
+        # echo $val
+        printf "$INFO[%16s] $END" $val
+        $PROGRAM --bin $file --xlen x64 --end-inst 0x0000006b
+    done
+
+    printf "$INFO===RV64I CUSTOM CPU TESTS===$END\n"
+    for file in $RV64I_CUST_TEST_BIN2; do
+        val=`eval "basename $file"`
+        # echo $val
+        printf "$INFO[%16s] $END" $val
+        $PROGRAM --bin $file --xlen x64 --end-inst 0x0000006b
     done
 }
 
