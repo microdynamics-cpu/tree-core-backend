72d132e3b0be3706fc4915026d046c994ed8c84a
maksyuki <maksyuki@126.com>
2022-04-30 08:57:45 +0800

    chore: modify the treecore unit test cases

--- src/simu/scripts/unit-test.sh
+++ src/simu/scripts/unit-test.sh
@@ -9,8 +9,9 @@ END="\033[0m"
 ROOT_PATH=$(dirname $(readlink -f "$0"))/../dependency
 RISCV_TESTS_PATH=${ROOT_PATH}/riscv-tests
 RISCV_TESTS_BIN_PATH=${RISCV_TESTS_PATH}/build/share/riscv-tests/isa
-CUST_TESTS_BIN_PATH1=/root/Desktop/chisel/tree-core-cpu/rtl/dependency/am/riscv-tests/build/
-CUST_TESTS_BIN_PATH2=/root/Desktop/chisel/tree-core-cpu/rtl/dependency/am/am-kernels/tests/cpu-tests/build/
+CPU_TESTS_BIN_PATH=${ROOT_PATH}/crt/am-kernels/tests/cpu-tests/build/
+# /root/Desktop/chisel/tree-core-cpu/rtl/dependency/am/am-kernels/tests/cpu-tests/build/
+# CUST_TESTS_BIN_PATH2=/root/Desktop/chisel/tree-core-cpu/rtl/dependency/am/am-kernels/tests/cpu-tests/build/
 
 PROGRAM=$(dirname $(readlink -f "$0"))/../target/debug/treecore_simu
 
@@ -19,8 +20,8 @@ RV32UI_P_TEST_BIN=`eval "find $RISCV_TESTS_BIN_PATH -type f -name \"rv32ui-p*\" 
 RV32UM_P_TEST_BIN=`eval "find $RISCV_TESTS_BIN_PATH -type f -name \"rv32um-p*\" ! -name \"*.dump\""`
 RV64UI_P_TEST_BIN=`eval "find $RISCV_TESTS_BIN_PATH -type f -name \"rv64ui-p*\" ! -name \"*.dump\""`
 RV64UM_P_TEST_BIN=`eval "find $RISCV_TESTS_BIN_PATH -type f -name \"rv64um-p*\" ! -name \"*.dump\""`
-RV64I_CUST_TEST_BIN1=`eval "find $CUST_TESTS_BIN_PATH1 -type f -name \"*-riscv64-mycpu.bin\" ! -name \"*.dump\""`
-RV64I_CUST_TEST_BIN2=`eval "find $CUST_TESTS_BIN_PATH2 -type f -name \"*-riscv64-mycpu.bin\" ! -name \"*.dump\""`
+CPU_TESTS_BIN=`eval "find $CPU_TESTS_BIN_PATH -type f -name \"*-riscv64-treecore.bin\" ! -name \"*.dump\""`
+# RV64I_CUST_TEST_BIN2=`eval "find $CUST_TESTS_BIN_PATH2 -type f -name \"*-riscv64-treecore.bin\" ! -name \"*.dump\""`
 
 unitTest() {
     printf "$INFO===RV32UI TESTS===$END\n"
@@ -52,20 +53,20 @@ unitTest() {
     done
 
     printf "$INFO===RV64I CUSTOM TESTS===$END\n"
-    for file in $RV64I_CUST_TEST_BIN1; do
+    for file in $CPU_TESTS_BIN; do
         val=`eval "basename $file"`
         # echo $val
         printf "$INFO[%16s] $END" $val
         $PROGRAM --bin $file --xlen x64 --end-inst 0x0000006b
     done
 
-    printf "$INFO===RV64I CUSTOM CPU TESTS===$END\n"
-    for file in $RV64I_CUST_TEST_BIN2; do
-        val=`eval "basename $file"`
+    # printf "$INFO===RV64I CUSTOM CPU TESTS===$END\n"
+    # for file in $RV64I_CUST_TEST_BIN2; do
+        # val=`eval "basename $file"`
         # echo $val
-        printf "$INFO[%16s] $END" $val
-        $PROGRAM --bin $file --xlen x64 --end-inst 0x0000006b
-    done
+        # printf "$INFO[%16s] $END" $val
+        # $PROGRAM --bin $file --xlen x64 --end-inst 0x0000006b
+    # done
 }
 
 
