6a5b14479891a7d218010eda4a0e688dc2995ad1
maksyuki <maksyuki@126.com>
2022-06-23 20:44:36 +0800

    chore: add sup deps in config file

--- src/server/src/main.rs
+++ src/server/src/main.rs
@@ -1,3 +1,6 @@
+#[macro_use]
+extern crate log;
+
 use clap::App;
 
 fn main() {
@@ -6,4 +9,7 @@ fn main() {
     .author(env!("CARGO_PKG_AUTHORS"))
     .about(env!("CARGO_PKG_DESCRIPTION"))
     .get_matches();
+
+    env_logger::init();
+    info!("Starting language server");
 }
