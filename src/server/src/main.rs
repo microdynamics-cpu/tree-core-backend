28a2b3996ca79518ce6a540b49416b7d42d4acac
maksyuki <maksyuki@126.com>
2022-06-23 18:30:02 +0800

    feat: add init cmd parser impl

---
+++ src/server/src/main.rs
@@ -0,0 +1,9 @@
+use clap::App;
+
+fn main() {
+    let _matches = App::new(env!("CARGO_PKG_NAME"))
+    .version(env!("CARGO_PKG_VERSION"))
+    .author(env!("CARGO_PKG_AUTHORS"))
+    .about(env!("CARGO_PKG_DESCRIPTION"))
+    .get_matches();
+}
