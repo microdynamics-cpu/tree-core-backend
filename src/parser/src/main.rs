4ee89abe846bccbfd12d0bf69bb43a0dbbebda9e
maksyuki <maksyuki@126.com>
2022-07-18 22:05:53 +0800

    feat: add time elapse count code

--- src/parser/src/main.rs
+++ src/parser/src/main.rs
@@ -1,14 +1,17 @@
-use std::env;
-use std::fs;
+use std::time::Instant;
+use std::{env, fs};
 use treecore_parser::vcd::vcd_main;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
     println!("In file {}", filename);
+    let start = Instant::now();
     let contents = fs::read_to_string(filename).expect("[error]read the file");
     // println!("With text:\n{}", contents);
     let res = vcd_main(contents.as_str());
+    let duration = start.elapsed();
+
     match res {
         Ok(v) => {
             println!("hdr: {:?}", v.1.hdr);
@@ -19,4 +22,5 @@ fn main() {
         }
         Err(_v) => panic!(),
     }
+    println!("Time elapsed in vcd_main() is: {:?}", duration);
 }
